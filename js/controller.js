43e910d3bc763bdbfb37b23e7c3ebba71f96efc6
Shawn Thompson <plansmash@me.com>
2016-03-14 15:41:37 -0400

    added data binding

---
+++ js/controller.js
@@ -0,0 +1,7 @@
+var bandList = angular.module('bandList', []);
+
+bandList.controller('bandListCtrl', ['$scope', '$http', function ($scope, $http) {
+  $http.get('./data/data.json').success(function(data) {
+      $scope.bands = data;
+  });
+}]);
